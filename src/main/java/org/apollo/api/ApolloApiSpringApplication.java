bbc5ce2f747d3aa10c490a2260f6af2c494675a8
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-07-08 16:41:41 -0300

    chore: initial spring project

---
+++ src/main/java/org/apollo/api/ApolloApiSpringApplication.java
@@ -0,0 +1,13 @@
+package org.apollo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloApiSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/apollo/api/ApolloApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apollo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
