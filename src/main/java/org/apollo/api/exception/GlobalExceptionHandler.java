2cdaff3acb31374351e40e8b680badecf2279ec6
Guilherme Guedes <guilhermeguedes350@gmail.com>
2026-08-23 19:19:01 -0300

    fix: handle database constraints

--- src/main/java/org/apollo/api/exception/GlobalExceptionHandler.java
+++ src/main/java/org/apollo/api/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.apollo.api.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,6 +21,15 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleIllegalArgument(IllegalArgumentException ex) {
         return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        return new ErrorResponse(
+                HttpStatus.CONFLICT.value(),
+                "Operação viola uma restrição de integridade dos dados"
+        );
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
